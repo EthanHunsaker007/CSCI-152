d7dcf7f310b6a2acb8263af1ac1a3e36ab52f181
Ethan Hunsaker <hunsaker.ethan1@gmail.com>
2026-01-23 11:40:48 -0700

    Implement JFrame business card

--- lab_one/src/App.java
+++ lab_one/src/App.java
@@ -1,5 +1,42 @@
+/*
+ * Author: Ethan Hunsaker
+ * Date: 01/23/2026
+ * Title: CSCI-152 Lab One
+ */
+
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Font font = new Font("Monospaced", Font.BOLD, 12);
+        String space = new String("&nbsp");
+
+
+        JFrame frame = new JFrame("Business Card");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("<html>" + "************************************" 
+                                  + "<br/>" + "*  Ethan Hunsaker" + space.repeat(19) + "*"
+                                  + "<br/>" + "*  Creative Writing & Game Design" + space.repeat(3) + "*"
+                                  + "<br/>" + "*  Emerald Green" + space.repeat(20) + "*"
+                                  + "<br/>" + "************************************" + "</html>");
+
+        label.setFont(font);
+        frame.add(label,BorderLayout.CENTER);
+
+        frame.setSize(300, 200);
+        
+        frame.setVisible(true);
+
+        // System.out.println("************************************");
+        // System.out.println("*  Ethan Hunsaker                  *");
+        // System.out.println("*  Creative Writing & Game Design  *");
+        // System.out.println("*  Emerald Green                   *");
+        // System.out.println("************************************");
     }
 }

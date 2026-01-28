f3b45e81f9bbac089e2001be6cb8eef307d94e11
Ethan Hunsaker <hunsaker.ethan1@gmail.com>
2026-01-28 11:23:52 -0700

    Finish lab_two

---
+++ lab_two/src/App.java
@@ -0,0 +1,21 @@
+/*
+ * Author: Ethan Hunsaker
+ * Date: 01/26/2026
+ * Name: Lab Two
+ */
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int i = 0;
+        byte b = 0;
+        short s = 0;
+        long l = 0;
+        char c = 'c';
+        float f = 10.000f;
+        double d = 10.000f;
+        boolean t = true;
+
+        System.out.println("" + i + b + s + l + c + f + d + t);
+    }
+}
